a81a8e65aea5c565ccab68053281f8876d7a9b0c
GonzaloMigliore <gonzalomigliore@gmail.com>
2021-03-31 20:49:05 -0300

    cambio index

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const session = require('express-session');
 
 // Requiero el middleware para recordar el usuario en la vista
 
-const recordarUser = require ('./middlewares/recordarUser');
+const recordarUser = require ('./src/middlewares/recordarUser');
 // view engine setup
 app.set('views', path.join(__dirname,'..', 'views'));
 app.set('view engine', 'ejs');
@@ -29,17 +29,17 @@ app.use(cookieParser());
 //app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.resolve(__dirname, '..','public')))
+app.use(express.static(path.resolve(__dirname,'public')))
 
 // Requiero el middleware para recordar el usuario en la vista
 app.use(recordarUser);
 
 //requiero las rutas
-const webRouter = require('./routes/web');
-const userRouter = require('./routes/users');
-const patientsRouter = require('./routes/patients')
-const apointmentsRouter = require('./routes/apointments')
-const adminUsersRouter = require('./routes/adminUsers')
+const webRouter = require('./src/routes/web');
+const userRouter = require('./src/routes/users');
+const patientsRouter = require('./src/routes/patients')
+const apointmentsRouter = require('./src/routes/apointments')
+const adminUsersRouter = require('./src/routes/adminUsers')
 app.use(webRouter);
 app.use(userRouter);
 app.use(patientsRouter);
