d0249d5624a1027211ba05fce0ef089bc04eb7ce
GonzaloMigliore <gonzalomigliore@gmail.com>
2021-05-28 11:42:33 -0300

    cambio img index

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,22 +1,22 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "clinica-centro-medico",
+    "username": "cms_database",
+    "password": "Gallo2573",
+    "database": "sistemcm_",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "port": 3306
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "clinica-centro-medico",
+    "username": "cms_database",
+    "password": "Gallo2573",
+    "database": "sistemcm_",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
+    "username": "cms_database",
+    "password": "Gallo2573",
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
