bd016d193c7ec5b3b43e80f938cd772c445e327d
GonzaloMigliore <gonzalomigliore@gmail.com>
2022-04-04 20:25:26 -0300

    cambio ultima entrega yavu

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,5 +1,29 @@
 module.exports = {
   "development": {
+    "username": "cms_database",
+    "password": "Gallo2573",
+    "database": "sistemcm_",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port": 3306
+  },
+  "test": {
+    "username": "cms_database",
+    "password": "Gallo2573",
+    "database": "sistemcm_",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "cms_database",
+    "password": "Gallo2573",
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+/* module.exports = {
+  "development": {
     "username": "root",
     "password": null,
     "database": "cms",
@@ -21,4 +45,4 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
-}
+} */
